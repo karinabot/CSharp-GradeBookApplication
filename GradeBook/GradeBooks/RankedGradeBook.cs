74c5322fcb3b518503b3dd1cc415375e9d3a8c1d
Karina <karina_ingeborg@hotmail.com>
2019-10-10 12:29:20 +0200

    completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -51,6 +51,36 @@ namespace GradeBook.GradeBooks
 
         }
 
+        public override void CalculateStatistics()
+        {
+           if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a students's overall grade.");
+                return;
+            }
+           else
+            {
+                base.CalculateStatistics();
+            }
+
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calclulate a student's overall grade.");
+                return;
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+
+            }
+
+        }
+
+
 
     }
 }
